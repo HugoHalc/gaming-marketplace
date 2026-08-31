bcd0a345bc28039824e5a92ef86610aab01c71db
HugoHalc <hitzveneno@gmail.com>
2026-08-31 11:35:35 -0600

    Phase 15J - Rocket League Service Navigation System

--- src/lib/supabase/browser.ts
+++
@@ -1,7 +0,0 @@
-import { createBrowserClient } from "@supabase/ssr";
-import { getPublicSupabaseEnv } from "./env";
-
-export function createAuthBrowserClient() {
-  const { url, publishableKey } = getPublicSupabaseEnv();
-  return createBrowserClient(url, publishableKey);
-}
